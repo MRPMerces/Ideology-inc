1e0515e4f73fa0b5664e9ac10bd55b55627e1b71
Fjelldar <85874867+Fjelldar@users.noreply.github.com>
2021-12-10 13:07:17 +0100

    lagt til et library som lar oss animere ui elementer på en mere effektiv måte, sletter eksempeler som ikke er nødvendig

--- Assets/UI/AutomaticVerticalSize.cs
+++ Assets/UI/AutomaticVerticalSize.cs
@@ -2,22 +2,28 @@
 using System.Collections;
 
 [ExecuteInEditMode]
-public class AutomaticVerticalSize : MonoBehaviour {
+public class AutomaticVerticalSize : MonoBehaviour
+{
 
-	public float childHeight = 35f;
+	public float childHeight = 10f;
+	public float childWidth = 175f;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		AdjustSize();
 	}
 
-	void Update() {
+	void Update()
+	{
 		AdjustSize();
 	}
-	
-	public void AdjustSize() {
+
+	public void AdjustSize()
+	{
 		Vector2 size = this.GetComponent<RectTransform>().sizeDelta;
 		size.y = this.transform.childCount * childHeight;
+		size.x = this.transform.childCount * childWidth;
 		this.GetComponent<RectTransform>().sizeDelta = size;
 	}
 }
